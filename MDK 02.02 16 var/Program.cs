897a12986171e2cf9576b844b80a0566c376500d
Cab38-01 <Cab38-01@03-C38-01>
2021-06-18 09:00:23 +0300

    Добавьте файлы проекта.

---
+++ MDK 02.02 16 var/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MDK_02._02_16_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
